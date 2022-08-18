d17e1ba45614f9e5156a0815a74324156113a6a5
Tan Bui <ductan2911@gmail.com>
2022-08-18 22:57:33 +0700

    added neural network implementation in c++

--- neural_network.cpp
+++ neural_network.cpp
@@ -3,15 +3,25 @@
 using namespace std;
 typedef long double ld;
 
+const int SEEDS = 20220820;
+// mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng(SEEDS);
+
 class Matrix 
 {
     public: 
         int m, n;
         vector< vector<ld> > vals;
     public: 
-        Matrix(int _m, int _n): m(_m), n(_n)
+        Matrix(int _m, int _n, bool rand = false): m(_m), n(_n)
         {
             vals.resize(m, vector<ld>(n, 0.0l));
+            if (rand)
+            {
+                for (int i = 0; i < m; ++i)
+                    for (int j = 0; j < n; ++j)
+                        vals[i][j] = uniform_real_distribution<ld>(0.0l, 1.0l)(rng);
+            }
         }
 
         Matrix operator*(const ld& scalar)
@@ -67,9 +77,121 @@ class Matrix
         }
 };
 
+// sigmoid = 1 / (1 + exp(-z)
+// relu = max(0, z)
+// softmax = exp(z) / sum(exp(z))
 
-
-int main()
+Matrix sigmoid(const Matrix& z)
 {
+    Matrix res(z.m, z.n);
+    for (int x = 0; x < z.m; ++x)
+        for (int y = 0; y < z.n; ++y)
+            res.vals[x][y] = 1.0l / (1.0l + exp(-z.vals[x][y]));
+    return res;
+}
 
+Matrix relu(const Matrix& z)
+{
+    Matrix res(z.m, z.n);
+    for (int x = 0; x < z.m; ++x)
+        for (int y = 0; y < z.n; ++y)
+            res.vals[x][y] = max(0.0l, z.vals[x][y]);
+    return res;
+}
+
+Matrix softmax(const Matrix& z)
+{
+    Matrix res(z.m, z.n);
+    for (int x = 0; x < z.m; ++x)
+    {
+        ld sum = 0.0l;
+        for (int y = 0; y < z.n; ++y)
+            sum += exp(z.vals[x][y]);
+        for (int y = 0; y < z.n; ++y)
+            res.vals[x][y] = exp(z.vals[x][y]) / sum;
+    }
+    return res;
+}
+
+Matrix forward_pass(Matrix& val, Matrix& w, Matrix& b)
+{
+    Matrix z = val * w + b;
+    return relu(z);
+}
+
+void dropout(Matrix& val, ld keep_rate)
+{
+    for (int x = 0; x < val.m; ++x)
+        for (int y = 0; y < val.n; ++y)
+            if (uniform_real_distribution<ld>(0.0l, 1.0l)(rng) > keep_rate) val.vals[x][y] = 0.0l;
+            else val.vals[x][y] /= keep_rate;
+}
+
+Matrix final_layer(Matrix& val, Matrix& w, Matrix& b)
+{
+    Matrix z = val * w + b;
+    return softmax(z);
+}
+
+int layers = 3, moves;
+int dim[3] = {1600, 256, 10};
+vector< pair<Matrix, Matrix> > param_layer;
+
+int main(int argc, char** argv)
+{
+    if (argc != 3) return 0;
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    // get_params
+    freopen("params.inp", "r", stdin);
+    for (int l = 1; l < layers; ++l)
+    {
+        Matrix w(dim[l - 1], dim[l]), b(1, dim[l]);
+        for (int i = 0; i < dim[l - 1]; ++i)
+            for (int j = 0; j < dim[l]; ++j)
+                cin >> w.vals[i][j];
+        for (int j = 0; j < dim[l]; ++j) cin >> b.vals[0][j];
+        param_layer.push_back(make_pair(w, b));
+    }
+    fclose(stdin);
+    // query
+    int l = atoi(argv[1]), r = atoi(argv[2]);
+    int successes = 0;
+    vector<int> wrong_ans;
+    for (int id = l; id <= r; ++id) 
+    {
+        ostringstream ss; ss << setw(5) << setfill('0') << id;
+        ifstream f_data("test/" + ss.str() + ".inp"); ifstream f_label("test/" + ss.str() + ".ans");
+        moves = layers - 1;
+        Matrix img(1, dim[0]);
+        for (int i = 0; i < 40; ++i)
+        {
+            string s; f_data >> s;
+            for (int j = 0; j < 40; ++j) img.vals[0][i * 40 + j] = s[j] - '0';
+        }
+        int answer; f_label >> answer;
+        for (int i = 0; i < moves - 1; ++i)
+        {
+            img = forward_pass(img, param_layer[i].first, param_layer[i].second);
+            dropout(img, 0.5l);
+        }
+        img = final_layer(img, param_layer[moves - 1].first, param_layer[moves - 1].second);
+        int output = 0;
+        for (int d = 1; d < 10; ++d) 
+            if (img.vals[0][d] > img.vals[0][output]) output = d;
+        cout << "\033[36m";
+        for (int d = 0; d < 10; ++d) cout << fixed << setprecision(9) << d << " | " << img.vals[0][d] * 100.0l << endl;
+        if (answer == output)
+        {
+            successes++;
+            cout << "\033[32m" << "Image #" << id << " | Pred = " << output << ", Answer = " << answer << " | " << "SUCCESS" << endl;
+        }
+        else
+        {
+            wrong_ans.emplace_back(id);
+            cout << "\033[31m" << "Image #" << id << " | Pred = " << output << ", Answer = " << answer << " | " << "FAILURE" << endl;
+        }
+        f_data.close(); f_label.close();
+    }
+    cout << "\033[0m" << "Accuracy = " << (ld)successes / (ld)(r - l + 1) * 100.0l << "%" << " | " << successes << " / " << r - l + 1 << endl;
+    cout << "Wrong Preds = "; for (int& v : wrong_ans) cout << v << " ";
 }
